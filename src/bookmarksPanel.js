bc4770b90c334e80149d59029765313c16ddf133
Pluto <devvaannsh@gmail.com>
2025-06-14 01:38:56 +0530

    feat: show no bookmarks message in the panel when no bookmarks are present

--- src/bookmarksPanel.js
+++ src/bookmarksPanel.js
@@ -9,10 +9,28 @@
 define(function (require, exports, module) {
     const WorkspaceManager = brackets.getModule("view/WorkspaceManager");
 
+    const BookmarksList = require("./bookmarksList");
+    const Helper = require("./helper");
+
     const panelHtml = require("text!../htmlContent/panel.html");
 
     const BOOKMARKS_PANEL_ID = "bookmarks.panel";
     let BookmarksPanel = null; // this will hold the panel reference
+
+
+    /**
+     * This function is to display the bookmarks list if there are any bookmarks
+     * otherwise it calls the required function to show the message that there are no bookmarks
+     */
+    function _showBookmarksList() {
+        const bookmarksList = BookmarksList.getBookmarksList();
+
+        if (Object.keys(bookmarksList).length === 0) { // if no bookmarks are added
+            Helper.showNoBookmarksMessage();
+        } else {
+            Helper.hideNoBookmarksMessage();
+        }
+    }
 
     /**
      * This function is called when the bookmarks toolbar icon is clicked for the first time
@@ -21,6 +39,7 @@ define(function (require, exports, module) {
     function _createBookmarksPanel() {
         BookmarksPanel = WorkspaceManager.createPluginPanel(BOOKMARKS_PANEL_ID, $(panelHtml), 200, $(), 400);
         BookmarksPanel.show();
+        _showBookmarksList();
     }
 
     /**
@@ -28,13 +47,16 @@ define(function (require, exports, module) {
      * it is used to show/hide the panel
      */
     function toggleBookmarksPanel() {
-        if (!BookmarksPanel) { // if the panel is not yet created even once, create it
+        if (!BookmarksPanel) {
+            // if the panel is not yet created even once, create it
             _createBookmarksPanel();
-        } else { // else we just want to toggle the visibility
+        } else {
+            // else we just want to toggle the visibility
             if (BookmarksPanel.isVisible()) {
                 BookmarksPanel.hide();
             } else {
                 BookmarksPanel.show();
+                _showBookmarksList();
             }
         }
     }
--- src/helper.js
+++ src/helper.js
@@ -11,5 +11,31 @@ define(function (require, exports, module) {
         return $("<div>").addClass("bookmark-icon").html(bookmarkSvg)[0];
     }
 
+    /**
+     * This function is to show the 'No bookmarks added yet' message
+     * and hide the bookmarks wrapper
+     */
+    function showNoBookmarksMessage() {
+        const $noBookmarksMessage = $("#no-bookmarks-wrapper");
+        const $bookmarksMessage = $("#bookmarks-wrapper");
+
+        $noBookmarksMessage.removeClass("hidden");
+        $bookmarksMessage.addClass("hidden");
+    }
+
+    /**
+     * This function is to show the bookmarks wrapper
+     * and hide the 'no bookmarks message'
+     */
+    function hideNoBookmarksMessage() {
+        const $noBookmarksMessage = $("#no-bookmarks-wrapper");
+        const $bookmarksMessage = $("#bookmarks-wrapper");
+
+        $noBookmarksMessage.addClass("hidden");
+        $bookmarksMessage.removeClass("hidden");
+    }
+
     exports.createBookmarkMarker = createBookmarkMarker;
+    exports.showNoBookmarksMessage = showNoBookmarksMessage;
+    exports.hideNoBookmarksMessage = hideNoBookmarksMessage;
 });
